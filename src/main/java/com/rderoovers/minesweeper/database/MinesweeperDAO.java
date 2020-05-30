1dbc9b984fa6473d353ff90c9b2dbedfa06e0247
Raúl De Roovers <nari77@gmail.com>
2020-05-30 11:33:27 -0300

    - Refactoring common code.

--- src/main/java/com/rderoovers/minesweeper/database/MinesweeperDAO.java
+++ src/main/java/com/rderoovers/minesweeper/database/MinesweeperDAO.java
@@ -9,11 +9,23 @@ import java.sql.*;
 
 public class MinesweeperDAO {
 
-    public void createGame(MinesweeperGameDBO minesweeperGame) throws SQLException, URISyntaxException, JsonProcessingException {
+    private String createJSON(MinesweeperGameDBO minesweeperGameDBO) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(minesweeperGameDBO);
+        return json;
+    }
+
+    private MinesweeperGameDBO createMinesweeperGameDBO(String json, long gameId) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        MinesweeperGameDBO minesweeperGameDBO = mapper.readValue(json, MinesweeperGameDBO.class);
+        minesweeperGameDBO.setId(gameId);
+        return minesweeperGameDBO;
+    }
+
+    public void createGame(MinesweeperGameDBO minesweeperGameDBO) throws SQLException, URISyntaxException, JsonProcessingException {
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO GAME (game_body, user_id, created_datetime, modified_datetime) VALUES (?,?,?,?) RETURNING game_id");
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(minesweeperGame);
+        String json = this.createJSON(minesweeperGameDBO);
         preparedStatement.setString(1, json);
         preparedStatement.setInt(2, 1);
         Date createdDate = new Date(System.currentTimeMillis());
@@ -23,8 +35,7 @@ public class MinesweeperDAO {
         try {
             rs.next();
             long gameId = rs.getLong(1);
-            minesweeperGame.setId(gameId);
-
+            minesweeperGameDBO.setId(gameId);
         }
         catch (Exception e) {
             throw new SQLException("Creating game failed, no ID obtained.");
@@ -32,11 +43,10 @@ public class MinesweeperDAO {
         connection.close();
     }
 
-    public void updateGame(MinesweeperGameDBO minesweeperGameDBO) throws URISyntaxException, SQLException, JsonProcessingException {
+    public void updateGame(MinesweeperGameDBO minesweeperGameDBO) throws SQLException, JsonProcessingException {
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("UPDATE GAME SET game_body = ?, modified_datetime = ? WHERE game_id = ?");
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(minesweeperGameDBO);
+        String json = this.createJSON(minesweeperGameDBO);
         preparedStatement.setString(1, json);
         Date createdDate = new Date(System.currentTimeMillis());
         preparedStatement.setDate(2, createdDate);
@@ -57,21 +67,21 @@ public class MinesweeperDAO {
         }
     }
 
-    private static Connection getConnection() throws URISyntaxException, SQLException {
+    private static Connection getConnection() throws SQLException {
         String url = "jdbc:postgresql://ec2-18-210-214-86.compute-1.amazonaws.com:5432/df07oro9fp521j?user=lwlxavyzarlohc&password=377f80cec2979296388b29b9cc3d612bb3de07b40b0bec58c51bf6e9b8947318&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
         return DriverManager.getConnection(url);
     }
 
-    public MinesweeperGameDBO loadGame(long gameId) throws URISyntaxException, SQLException, JsonProcessingException {
+    public MinesweeperGameDBO loadGame(long gameId) throws SQLException, JsonProcessingException {
         Connection connection = getConnection();
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT game_body, user_id FROM game WHERE game_id = ?");
         preparedStatement.setLong(1, gameId);
         ResultSet resultSet = preparedStatement.executeQuery();
         resultSet.next();
         String json = resultSet.getString(1);
-        ObjectMapper mapper = new ObjectMapper();
-        MinesweeperGameDBO minesweeperGameDBO = mapper.readValue(json, MinesweeperGameDBO.class);
-        minesweeperGameDBO.setId(gameId);
+        connection.close();
+        MinesweeperGameDBO minesweeperGameDBO = this.createMinesweeperGameDBO(json, gameId);
         return minesweeperGameDBO;
     }
+
 }
